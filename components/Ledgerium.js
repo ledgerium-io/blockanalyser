51cdfb704de283e41fe83456da65132f3f254fba
Charan <saicharan.madina959@gmail.com>
2019-06-12 13:34:47 +0530

    Displays validators balances for every 10 blocks

--- components/Ledgerium.js
+++ components/Ledgerium.js
@@ -1,12 +1,61 @@
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider("http://125.254.27.14:28545"));
 const chalk = require('chalk')
+const request = require('request');
+const querystring = require('querystring');
 
 class Ledgerium {
   constructor() {
     this.latestBlock = 0
     this.miner = ''
-    this.validators = []
+    // this.validators = []
+    this.oldBlock;
+    this.diff = 10;
+    this.count = 0;
+    this.balances = {};
+  }
+
+  getNewStats() {
+    
+    web3.eth.getBlockNumber()
+      .then((blockNumber) => {
+        console.log(blockNumber)
+
+        this.getBalances(blockNumber)
+          .then((result) => {
+          })
+        
+        //Store block number for the first time
+        if ( this.count === 0 ) {
+          this.count = 1;          
+          this.oldBlock = blockNumber;
+        } else {
+          //If difference is 10, print balances
+          if(blockNumber === this.oldBlock+this.diff){
+            this.oldBlock = blockNumber;
+            console.log("---", this.balances)
+            //Empty after 10 blocks
+            this.balances = {} 
+          }
+        }
+      })
+  }
+
+  getBalances(blockNumber) {
+    return new Promise ((resolve, reject) => {
+
+      this.getValidators()
+      .then((validators) => {
+        for(let i=0; i<validators.length; i++) {
+          web3.eth.getBalance(validators[i])
+          .then(balance => {
+            if(this.balances[validators[i]] == undefined) this.balances[validators[i]] = {};
+            this.balances[validators[i]][blockNumber] = web3.utils.fromWei(balance, 'ether') + ' XLG';
+          })
+        }
+        resolve(this.balances);            
+      })
+    })
   }
 
 
@@ -35,6 +84,7 @@ class Ledgerium {
       return new Promise((resolve, reject) => {
         web3.eth.getBlock('latest')
           .then(block => {
+            console.log(istanbul.getValidators())
             if(!block) return reject("Invalid block")
             if(block.number === 0) reject ("Block not found")
             this.latestBlock = block.number
@@ -50,6 +100,29 @@ class Ledgerium {
             reject(error)
           })
       })
+  }
+
+  getValidators() {
+    return new Promise ((resolve, reject) => {
+      
+      request({
+        url: 'http://125.254.27.14:28545/',
+        method: 'POST',
+        json: { 
+          jsonrpc: "2.0", 
+          method: "istanbul_getValidators", 
+          params: [], 
+          id: new Date().getTime()
+        },
+      }, (err, res) => {
+        if(err) {
+          console.log('Error in getValidators', err)
+        } else {
+          resolve(res.body.result);
+        }
+      })
+    })
+
   }
 
 }
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Ledgerium = require('./components/Ledgerium')
 const ledgerium = new Ledgerium()
 
 setInterval(() => {
-  process.stdout.write("\u001b[2J\u001b[0;0H")
-  ledgerium.getStats()
+  // process.stdout.write("\u001b[2J\u001b[0;0H")
+  ledgerium.getNewStats()
 },5000)
